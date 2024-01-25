eb7de9554dde6aabf1b9190b1f3905f3ed92ae4d
Andy Chen <chen.andy18@gmail.com>
2024-01-25 14:34:23 -0800

    Time: 61 ms (95.43%), Space: 58.9 MB (9.97%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,8 +3,8 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let minPrice = prices[0]; //2
-    let maxProfit = 0; //4
+    let minPrice = prices[0];
+    let maxProfit = 0;
     for (let i = 1; i < prices.length; i++) {
         if (prices[i] < minPrice) {
             minPrice = prices[i];
