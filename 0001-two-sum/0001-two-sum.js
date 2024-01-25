b4fe46ce63dbc7d612d7c7d93ef074086ee83061
Andy Chen <chen.andy18@gmail.com>
2024-01-25 12:51:07 -0800

    Time: 56 ms (80.84%), Space: 49.7 MB (6.24%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,10 +4,16 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    // create a map
     let map = {};
+    // loop through nums
     for (let i = 0; i < nums.length; i++) {
+        // create a variable to store value of target - nums[i]
         let possible = target - nums[i];
+        // check map to see if variable value exist
+        // if not then store key as nums[i] and value as i
         if (map[possible] === undefined) map[nums[i]] = i;
+        // if it does then return i and map[variable value]
         else return [i, map[possible]];
     }
 };
