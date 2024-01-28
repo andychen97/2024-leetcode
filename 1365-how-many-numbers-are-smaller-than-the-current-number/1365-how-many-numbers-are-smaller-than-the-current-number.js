718a959b83016673766bf87f7f49741eb4bfda2a
Andy Chen <chen.andy18@gmail.com>
2024-01-28 01:44:37 -0800

    Time: 72 ms (49.46%), Space: 51.9 MB (5.47%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
@@ -7,7 +7,7 @@ var smallerNumbersThanCurrent = function(nums) {
     for (let i = 0; i < nums.length; i++) {
         let count = 0;
         for (let j = 0; j < nums.length; j++) {
-            if (nums[j] < nums[i]) count++
+            if (nums[j] < nums[i]) count++;
         }
         counter.push(count);
     }
