49ebf2065a194d3a5efee6e754e8f9f8e76d47dc
Andy Chen <chen.andy18@gmail.com>
2024-01-30 05:40:12 -0800

    Time: 54 ms (79.36%), Space: 50.3 MB (19.90%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -7,11 +7,11 @@ var canPlaceFlowers = function(flowerbed, n) {
     let count = 0;
     const length = flowerbed.length;
     for (let i = 0; i < length; i++) {
-        if ((i === 0 || flowerbed[i-1] === 0) && 
-            (i === length - 1 || flowerbed[i+1] === 0) && 
+        if ((i === 0 || flowerbed[i - 1] === 0) && 
+            (i === length - 1 || flowerbed[i + 1] === 0) && 
             flowerbed[i] !== 1) {
-            count++;
             flowerbed[i] = 1;
+            count++;
         }
     }
     return count >= n;
