7ea4a28170dba09b9db9643f0731bd994da36df1
Andy Chen <chen.andy18@gmail.com>
2024-01-30 05:47:39 -0800

    Time: 64 ms (22.03%), Space: 50.1 MB (14.25%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
@@ -4,15 +4,10 @@
  * @return {boolean[]}
  */
 var kidsWithCandies = function(candies, extraCandies) {
-    let highest = 0;
-    for (let i = 0; i < candies.length; i++) {
-        if (candies[i] >= highest) highest = candies[i];
-    }
-    
+    let highest = Math.max(...candies);
     let arr = [];
-    for (let j = 0; j < candies.length; j++) {
-        if (candies[j] + extraCandies >= highest) arr.push(true);
-        else arr.push(false);
+    for (let i = 0; i < candies.length; i++) {
+        arr.push(candies[i] + extraCandies >= highest);
     }
     return arr;
 };
