9946153fefc8341d90cb7f3663c6e61e66c12ce1
Andy Chen <chen.andy18@gmail.com>
2024-02-01 08:01:13 -0800

    Time: 78 ms (56.25%), Space: 53.3 MB (18.90%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+
+var moveZeroes = function(nums) {
+    let ind = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[ind] = nums[i];
+            ind++;
+        }
+    }
+    for (let i = ind; i < nums.length; i++) {
+        nums[i] = 0;
+    }
+};
